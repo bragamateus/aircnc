a94e31471540586b2a3335a7e5743db0489b96ce
Mateus Braga <braga.mateus16@gmail.com>
2019-10-01 21:48:43 -0300

    Adicionar um spot

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,7 +1,18 @@
 //index, show, store, update, destroy
+const User = require('../models/User')
 
 module.exports = {
-    store(req, res){
-        return res.json({message: "Hello!!!"})
+    async store(req, res){
+        const { email } = req.body;
+
+        let user = await User.findOne({email});
+
+        if (!user){
+            user = await User.create({ email });
+        }
+        
+      
+
+        return res.json(user);
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const SessionController = require('./controllers/SessionController')
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
+const upload =multer(uploadConfig);
 
 //req.query = Acessar query params (para filtros)
 //req.paramns = Acessar route params (para edição e delete)
 //req.body = acessar corpo da requisição (para criação ou edição de registros)
 
-routes.post('/users', SessionController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 module.exports = routes;
