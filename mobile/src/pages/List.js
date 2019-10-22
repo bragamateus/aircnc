fbf2219d4ba59ef6563bc035eb220aa7d034c3c6
Your Name <braga.mateus16@gmail.com>
2019-10-21 23:14:22 -0300

    Criando o primeiro componente reactive-native

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import {Text, AsyncStorage } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, AsyncStorage, Image } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
@@ -12,6 +16,23 @@ export default function List() {
         })
     }, []);
 
-    return <Text>{techs}</Text>
-
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            <SpotList />
+        </SafeAreaView>
+     )  
 } 
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+
+    logo: {
+        height:32,
+        resizeMode: "contain",
+        alignSelf: 'center',
+        marginTop: 30
+    }
+}); 
